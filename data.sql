05d1c04b8b7247acb6b51d4c7c13671fffaef2c7
ajaychandrathati <ajaychandrathati@gmail.com>
2026-07-01 12:08:54 +0530

    Create secure_login database and users table

---
+++ data.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE secure_login;
+
+USE secure_login;
+
+CREATE TABLE users (
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+username VARCHAR(50) UNIQUE NOT NULL,
+
+email VARCHAR(100) UNIQUE NOT NULL,
+
+password TEXT NOT NULL,
+
+secret TEXT,
+
+twofa BOOLEAN DEFAULT FALSE
+
+);
